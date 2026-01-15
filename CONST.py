c718c7f3543f758f4c15a3ba0ae7d5f4d1050198
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2026-01-14 16:08:14 -0800

    Add namespaces. Add comments about what each cpc points to, commenting out the ones that I'm not actually interested in

--- CONST.py
+++ CONST.py
@@ -1,50 +1,58 @@
 
 CPC_lui_2022 = [
-    "A61B5/7264",
-    "A61B5/7267",
-    "A63F13/67",
-    "B23K31/006",
-    "B25J9/161",
+    # "A61B5/7264", # medical
+    # "A61B5/7267",
+    # "A63F13/67",
+    "B23K31/006", # SOLDERING OR UNSOLDERING specially adapted for particular articles or purposes
+    "B25J9/161", # Program-controlled manipulators
     "B25J9/163",
-    "B29C66/965",
-    "B29C2945/76946",
+    "B29C66/965",# General aspects of processes or apparatus for joining preformed parts
+    # using artificial neural networks *
+    "B29C2945/76946", # Indexing scheme relating to injection moulding
     "B29C2945/76949",
     "B29C2945/76979",
-    "B60G2600/1876",
-    "B60G2600/1878",
-    "B60L2260/46",
-    "B60T8/174",
-    "B60T2210/122",
-    "B64G2001/247",
-    "B65H2557/38",
-    "B66B7/043",
-    "B66B7/045",
-    "E21B2041/0028",
-    "F01N2900/0402",
-    "F02D41/1405",
-    "F03D7/046",
-    "F05B2270/709",
-    "F05D2270/709",
-    "F16H2059/086",
-    "F16H2061/0084",
-    "F16H2061/0087",
-    "G01N29/4481",
-    "G01N30/8662",
-    "G01N33/0034",
+    # "B60G2600/1876", Vehicular
+    # "B60G2600/1878",
+    # "B60L2260/46",
+    # "B60T8/174",
+    # "B60T2210/122",
+    # "B64G2001/247",
+    "B65H2557/38", # means for neural adaptive control *
+    # "B66B7/043", # elevators
+    # "B66B7/045",
+    # "E21B2041/0028", # mining
+    "F01N2900/0402", #  Details of electrical control or of the monitoring of the
+    # exhaust gas treating apparatus
+    "F02D41/1405", # compustion engines
+    "F03D7/046", # Controlling wind motors
+    "F05B2270/709", # machine components
+    "F05D2270/709", # gas turbines
+    # "F16H2059/086", # GEARING - Adaptive mode, e.g. learning from the driver *
+    # "F16H2061/0084", # GEARING - nural adaptors *
+    # "F16H2061/0087", # GEARING - Adaptive control, e.g. the control parameters
+    # adapted by learning *
+    "G01N29/4481", # materials science
+    "G01N30/8662", # ion exchange
+    "G01N33/0034", # Investigating or analysing materials by specific methods not covered
+    # by groups
+     # G01N 1/00 - G01N 31/00
     "G01N2201/1296",
-    "G01R31/2846",
+    "G01R31/2846", # Arrangements for testing electric propertie
     "G01R31/3651",
-    "G01S7/417",
-    "G05B13/027",
+    "G01S7/417", # data exchange
+    "G05B13/027", # Adaptive control systems, i.e. systems automatically adjusting themselves
+    # to have a performance which is optimum according to some preassigned criterion
+    # (G05B 19/00 takes precedence)
     "G05B13/028",
     "G05B13/0285",
     "G05B13/029",
     "G05B13/0295",
-    "G05B23/0229",
+    "G05B23/0229", # Testing or monitoring of control systems or parts thereof
     "G05B23/024",
     "G05B23/0254",
     "G05B23/0281",
-    "G05B2219/13111",
+    "G05B2219/13111", # G05B: CONTROL OR REGULATING SYSTEMS IN GENERAL; FUNCTIONAL ELEMENTS OF
+    # SUCH SYSTEMS; MONITORING OR TESTING ARRANGEMENTS FOR SUCH SYSTEMS OR ELEMENTS
     "G05B2219/13166",
     "G05B2219/21002",
     "G05B2219/23253",
@@ -127,13 +135,15 @@ CPC_lui_2022 = [
     "G05B2219/42149",
     "G05B2219/42287",
     "G05B2219/49065",
-    "G05D1/0088",
-    "G05D1/0221",
-    "G06F7/023",
-    "G06F11/1476",
+    # "G05D1/0088", # automous vehicles
+    # "G05D1/0221",
+    "G06F7/023", # Methods or arrangements for processing data by operating upon the order
+    # or content of the data handled
+    "G06F11/1476", # Error detection; Error correction; Monitoring
     "G06F11/2257",
     "G06F11/2263",
-    "G06F15/18",
+    "G06F15/18", # Digital computers in general (details G06F 1/00 – G06F 13/00); Data
+    # processing equipment in general
     "G06F16/243",
     "G06F16/24522",
     "G06F16/3329",
@@ -153,13 +163,13 @@ CPC_lui_2022 = [
     "G06F19/345",
     "G06F19/707",
     "G06F2207/4824",
-    "G06K7/1482",
-    "G06K9/6256",
+    "G06K7/1482", # Methods or arrangements for sensing record carriers
+    "G06K9/6256", # GRAPHICAL DATA READING
     "G06K9/6264",
     "G06K9/6269",
     "G06K9/627",
     "G06K9/6273",
-    "G06N3/004",
+    "G06N3/004", # Computing arrangements based on biological models
     "G06N3/008",
     "G06N3/02",
     "G06N3/0427",
@@ -171,71 +181,86 @@ CPC_lui_2022 = [
     "G06N3/08",
     "G06N3/084",
     "G06N3/086",
-    "G06N5",
+    "G06N5", # Computing arrangements using knowledge-based models
     "G06N5/00",
     "G06N5/02",
     "G06N5/043",
-    "G06N7/023",
+    "G06N7/023", # Computing arrangements based on specific mathematical models
     "G06N7/046",
-    "G06N20",
+    "G06N20", # Machine learning
     "G06N20/00",
     "G06N20/10",
     "G06N20/20",
-    "G06N99/005",
-    "G06T3/4046",
-    "G06T9/002",
-    "G06T2207/20081",
-    "G06T2207/20084",
-    "G07C2009/00849",
-    "G07C2009/00888",
-    "G07D7/2083",
-    "G08B29/186",
-    "G08G1/096888",
-    "G10H2250/311",
-    "G10K2210/3024",
-    "G10K2210/3038",
-    "G10L15/06",
-    "G10L15/144",
-    "G10L15/16",
-    "G10L15/18",
-    "G10L17/18",
-    "G10L25/30",
-    "G11B20/10518",
-    "G16B40",
-    "G16C20/70",
-    "G16H50/20",
-    "G21D3/007",
+    "G06N99/005", # Molecular computers, i.e. using inorganic molecules
+    "G06T3/4046", # Geometric image transformations in the plane of the image
+    "G06T9/002", # Image coding
+    # "G06T2207/20081", # Indexing scheme for image analysis or image enhancement
+    # "G06T2207/20084",
+    # "G07C2009/00849",
+    # "G07C2009/00888",
+    # "G07D7/2083", # pattern matching
+    "G08B29/186", #Prevention or correction of operating errors
+    # "G08G1/096888", # vehicular
+    "G10H2250/311", # Aspects of algorithms or signal processing methods without
+    # intrinsic musical character, yet specifically adapted for or used in electrophonic
+    # musical processing
+    # "G10K2210/3024", #  Details of active noise control
+    # "G10K2210/3038",
+    # "G10L15/06", # Speech recognition
+    # "G10L15/144",
+    # "G10L15/16",
+    # "G10L15/18",
+    # "G10L17/18", # Speech identification
+    # "G10L25/30", #""
+    # "G11B20/10518", # Signal processing not specific to the method of recording or reproducing
+    # "G16B40", # ICT specially adapted for biostatistics
+    # "G16C20/70",# Chemoinformatics
+    # "G16H50/20", # ICT specially adapted for medical diagnosis
+    "G21D3/007", #  Control of nuclear power plant
     "G21D2003/007",
-    "H01H2009/566",
+    "H01H2009/566", # ELECTRIC SWITCHES; RELAYS; SELECTORS; EMERGENCY PROTECTIVE DEVICES
     "H01H2047/009",
-    "H01J2237/30427",
-    "H01M8/04992",
-    "H02H1/0092",
-    "H02P21/0014",
+    "H01J2237/30427", # Discharge tubes exposing object to beam, e.g. for analysis treatment,
+    # etching, imaging
+    "H01M8/04992", # Fuel cells; Manufacture thereof
+    "H02H1/0092", # Details of emergency protective circuit arrangements
+    "H02P21/0014", # Arrangements or methods for the control of electric machines by vector
+    # control, e.g. by control of field orientation
     "H02P21/0025",
-    "H02P23/0018",
+    "H02P23/0018", #    Arrangements or methods for the control of AC motors characterised by a
+    # control method other than vector control
     "H02P23/0031",
-    "H03H2017/0208",
+    "H03H2017/0208", # Networks using elements or techniques not provided for in groups
+    # H03H 3/00 - H03H 21/00
     "H03H2222/04",
-    "H04L12/2423",
-    "H04L25/0254",
+    "H04L12/2423", # H04L 12/00  Data switching networks
+    "H04L25/0254", # Baseband systems
     "H04L25/03165",
-    "H04L41/16",
-    "H04L45/08",
+    "H04L41/16", # Arrangements for maintenance, administration or management of data switching
+    # networks, e.g. of packet switching networks
+    "H04L45/08", # Routing or path finding of packets in data switching networks
     "H04L45/36",
-    "H04L2012/5686",
+    "H04L2012/5686", # TRANSMISSION OF DIGITAL INFORMATION, e.g. TELEGRAPHIC COMMUNICATION
     "H04L2025/03464",
     "H04L2025/03554",
-    "H04N21/4662",
+    "H04N21/4662", # Selective content distribution, e.g. interactive television or video on
+    # demand [VOD]
     "H04N21/4663",
     "H04N21/4665",
     "H04N21/4666",
-    "H04Q2213/054",
+    "H04Q2213/054", # Indexing scheme relating to selecting arrangements in general and for
+    # multiplex systems
     "H04Q2213/13054",
     "H04Q2213/13343",
     "H04Q2213/343",
-    "H04R25/507",
-    "Y10S128/924",
-    "Y10S128/925",
-    "Y10S706"
+    # "H04R25/507", # Electric hearing aids
+    # "Y10S128/924", # Surgery
+    # "Y10S128/925",
+    "Y10S706" # Data processing: artificial intelligence
 ]
+
+NAMESPACES = {
+    'uscom': 'urn:us:gov:doc:uspto:common',
+    'uspat': 'urn:us:gov:doc:uspto:patent',
+    'com': 'http://www.wipo.int/standards/XMLSchema/ST96/Common'
+}
