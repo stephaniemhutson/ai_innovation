61f51311bfd2205e9eb192e8b56b9f6375573eac
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2026-02-23 17:52:46 -0800

    For citations, I wanted to capture everything, even if it means I capture unrelated things.
    The reason to do this is so that if I only manage to capture the narrow selection that I originally look at, then I might not
    be capturing the full impact.

--- CONST.py
+++ CONST.py
@@ -282,6 +282,289 @@ CPC_lui_2022 = [
     "H10D*", # Energy efficiency
 ]
 
+CPC_lui_2022__all = [
+    "A61B5/7264", # medical
+    "A61B5/7267",
+    "A63F13/67",
+    "B23K31/006", # SOLDERING OR UNSOLDERING specially adapted for particular articles or purposes
+    "B25J9/161", # Program-controlled manipulators
+    "B25J9/163",
+    "B29C66/965",# General aspects of processes or apparatus for joining preformed parts
+    # using artificial neural networks *
+    "B29C2945/76946", # Indexing scheme relating to injection moulding
+    "B29C2945/76949",
+    "B29C2945/76979",
+    "B60G2600/1876", # Vehicular
+    "B60G2600/1878",
+    "B60L2260/46",
+    "B60T8/174",
+    "B60T2210/122",
+    "B64G2001/247",
+    "B65H2557/38", # means for neural adaptive control *
+    "B66B7/043", # elevators
+    "B66B7/045",
+    "E21B2041/0028", # mining
+    "F01N2900/0402", #  Details of electrical control or of the monitoring of the
+    # exhaust gas treating apparatus
+    "F02D41/1405", # compustion engines
+    "F03D7/046", # Controlling wind motors
+    "F05B2270/709", # machine components
+    "F05D2270/709", # gas turbines
+    "F16H2059/086", # GEARING - Adaptive mode, e.g. learning from the driver *
+    "F16H2061/0084", # GEARING - nural adaptors *
+    "F16H2061/0087", # GEARING - Adaptive control, e.g. the control parameters
+    # adapted by learning *
+    "G01N29/4481", # materials science
+    "G01N30/8662", # ion exchange
+    "G01N33/0034", # Investigating or analysing materials by specific methods not covered
+    # by groups
+     # G01N 1/00 - G01N 31/00
+    "G01N2201/1296",
+    "G01R31/2846", # Arrangements for testing electric propertie
+    "G01R31/3651",
+    "G01S7/417", # data exchange
+    "G05B13/027", # Adaptive control systems, i.e. systems automatically adjusting themselves
+    # to have a performance which is optimum according to some preassigned criterion
+    # (G05B 19/00 takes precedence)
+    "G05B13/028",
+    "G05B13/0285",
+    "G05B13/029",
+    "G05B13/0295",
+    "G05B23/0229", # Testing or monitoring of control systems or parts thereof
+    "G05B23/024",
+    "G05B23/0254",
+    "G05B23/0281",
+    "G05B2219/13111", # G05B: CONTROL OR REGULATING SYSTEMS IN GENERAL; FUNCTIONAL ELEMENTS OF
+    # SUCH SYSTEMS; MONITORING OR TESTING ARRANGEMENTS FOR SUCH SYSTEMS OR ELEMENTS
+    "G05B2219/13166",
+    "G05B2219/21002",
+    "G05B2219/23253",
+    "G05B2219/23288",
+    "G05B2219/24086",
+    "G05B2219/25255",
+    "G05B2219/31351",
+    "G05B2219/31352",
+    "G05B2219/31353",
+    "G05B2219/31354",
+    "G05B2219/32193",
+    "G05B2219/32327",
+    "G05B2219/32329",
+    "G05B2219/32334",
+    "G05B2219/32335",
+    "G05B2219/33002",
+    "G05B2219/33013",
+    "G05B2219/33014",
+    "G05B2219/33015",
+    "G05B2219/33021",
+    "G05B2219/33024",
+    "G05B2219/33025",
+    "G05B2219/33026",
+    "G05B2219/33027",
+    "G05B2219/33028",
+    "G05B2219/33029",
+    "G05B2219/33033",
+    "G05B2219/33034",
+    "G05B2219/33035",
+    "G05B2219/33038",
+    "G05B2219/33039",
+    "G05B2219/33041",
+    "G05B2219/33044",
+    "G05B2219/33056",
+    "G05B2219/33065",
+    "G05B2219/33066",
+    "G05B2219/33295",
+    "G05B2219/33303",
+    "G05B2219/33321",
+    "G05B2219/33322",
+    "G05B2219/34066",
+    "G05B2219/34081",
+    "G05B2219/34082",
+    "G05B2219/36039",
+    "G05B2219/36456",
+    "G05B2219/39071",
+    "G05B2219/39072",
+    "G05B2219/39095",
+    "G05B2219/39268",
+    "G05B2219/39271",
+    "G05B2219/39276",
+    "G05B2219/39282",
+    "G05B2219/39283",
+    "G05B2219/39284",
+    "G05B2219/39286",
+    "G05B2219/39292",
+    "G05B2219/39294",
+    "G05B2219/39297",
+    "G05B2219/39298",
+    "G05B2219/39311",
+    "G05B2219/39312",
+    "G05B2219/39352",
+    "G05B2219/39372",
+    "G05B2219/39374",
+    "G05B2219/39376",
+    "G05B2219/39385",
+    "G05B2219/40107",
+    "G05B2219/40115",
+    "G05B2219/40408",
+    "G05B2219/40494",
+    "G05B2219/40496",
+    "G05B2219/40499",
+    "G05B2219/40528",
+    "G05B2219/40529",
+    "G05B2219/41054",
+    "G05B2219/42018",
+    "G05B2219/42135",
+    "G05B2219/42141",
+    "G05B2219/42142",
+    "G05B2219/42149",
+    "G05B2219/42287",
+    "G05B2219/49065",
+    "G05D1/0088", # automous vehicles
+    "G05D1/0221",
+    "G06F7/023", # Methods or arrangements for processing data by operating upon the order
+    # or content of the data handled
+    "G06F11/1476", # Error detection; Error correction; Monitoring
+    "G06F11/2257",
+    "G06F11/2263",
+    "G06F15/18", # Digital computers in general (details G06F 1/00 – G06F 13/00); Data
+    # processing equipment in general
+    "G06F16/243",
+    "G06F16/24522",
+    "G06F16/3329",
+    "G06F16/3344",
+    "G06F16/90332",
+    "G06F17/20",
+    "G06F17/2282",
+    "G06F17/28",
+    "G06F17/2881",
+    "G06F17/289",
+    "G06F17/30401",
+    "G06F17/3043",
+    "G06F17/30654",
+    "G06F17/30684",
+    "G06F17/30976",
+    "G06F19/24",
+    "G06F19/345",
+    "G06F19/707",
+    "G06F2207/4824",
+    "G06K7/1482", # Methods or arrangements for sensing record carriers
+    "G06K9/6256", # GRAPHICAL DATA READING
+    "G06K9/6264",
+    "G06K9/6269",
+    "G06K9/627",
+    "G06K9/6273",
+    "G06N3/004", # Computing arrangements based on biological models
+    "G06N3/008",
+    "G06N3/02",
+    "G06N3/0427",
+    "G06N3/0445",
+    "G06N3/0463",
+    "G06N3/0481",
+    "G06N3/049",
+    "G06N3/06",
+    "G06N3/08",
+    "G06N3/084",
+    "G06N3/086",
+    "G06N5/*", # Computing arrangements using knowledge-based models
+    "G06N5/00",
+    "G06N5/02",
+    "G06N5/043",
+    "G06N7/023", # Computing arrangements based on specific mathematical models
+    "G06N7/046",
+    "G06N20/*", # Machine learning
+    "G06N20/00",
+    "G06N20/10",
+    "G06N20/20",
+    "G06N99/005", # Molecular computers, i.e. using inorganic molecules
+    "G06T3/4046", # Geometric image transformations in the plane of the image
+    "G06T9/002", # Image coding
+    "G06T2207/20081", # Indexing scheme for image analysis or image enhancement
+    "G06T2207/20084",
+    "G07C2009/00849",
+    "G07C2009/00888",
+    "G07D7/2083", # pattern matching
+    "G08B29/186", #Prevention or correction of operating errors
+    # "G08G1/096888", # vehicular
+    "G10H2250/311", # Aspects of algorithms or signal processing methods without
+    # intrinsic musical character, yet specifically adapted for or used in electrophonic
+    # musical processing
+    "G10K2210/3024", #  Details of active noise control
+    "G10K2210/3038",
+    "G10L15/06", # Speech recognition
+    "G10L15/144",
+    "G10L15/16",
+    "G10L15/18",
+    "G10L17/18", # Speech identification
+    "G10L25/30", #""
+    "G11B20/10518", # Signal processing not specific to the method of recording or reproducing
+    "G16B40", # ICT specially adapted for biostatistics
+    "G16C20/70",# Chemoinformatics
+    "G16H50/20", # ICT specially adapted for medical diagnosis
+    "G21D3/007", #  Control of nuclear power plant
+    "G21D2003/007",
+    "H01H2009/566", # ELECTRIC SWITCHES; RELAYS; SELECTORS; EMERGENCY PROTECTIVE DEVICES
+    "H01H2047/009",
+    "H01J2237/30427", # Discharge tubes exposing object to beam, e.g. for analysis treatment,
+    # etching, imaging
+    "H01M8/04992", # Fuel cells; Manufacture thereof
+    "H02H1/0092", # Details of emergency protective circuit arrangements
+    "H02P21/0014", # Arrangements or methods for the control of electric machines by vector
+    # control, e.g. by control of field orientation
+    "H02P21/0025",
+    "H02P23/0018", #    Arrangements or methods for the control of AC motors characterised by a
+    # control method other than vector control
+    "H02P23/0031",
+    "H03H2017/0208", # Networks using elements or techniques not provided for in groups
+    # H03H 3/00 - H03H 21/00
+    "H03H2222/04",
+    "H04L12/2423", # H04L 12/00  Data switching networks
+    "H04L25/0254", # Baseband systems
+    "H04L25/03165",
+    "H04L41/16", # Arrangements for maintenance, administration or management of data switching
+    # networks, e.g. of packet switching networks
+    "H04L45/08", # Routing or path finding of packets in data switching networks
+    "H04L45/36",
+    "H04L2012/5686", # TRANSMISSION OF DIGITAL INFORMATION, e.g. TELEGRAPHIC COMMUNICATION
+    "H04L2025/03464",
+    "H04L2025/03554",
+    "H04N21/4662", # Selective content distribution, e.g. interactive television or video on
+    # demand [VOD]
+    "H04N21/4663",
+    "H04N21/4665",
+    "H04N21/4666",
+    "H04Q2213/054", # Indexing scheme relating to selecting arrangements in general and for
+    # multiplex systems
+    "H04Q2213/13054",
+    "H04Q2213/13343",
+    "H04Q2213/343",
+    "H04R25/507", # Electric hearing aids
+    "Y10S128/924", # Surgery
+    "Y10S128/925",
+    "Y10S706", # Data processing: artificial intelligence
+
+
+    # From ChatGPT -- cooling and energy efficiency
+    "F28D*", # Facility level efficiency
+    "F28F*", # DETAILS OF HEAT-EXCHANGE
+    "F28C*", # HEAT-EXCHANGE APPARATUS
+    "F24F5/*", # Air-conditioning systems or apparatus not covered by F24F 1/00
+    "F24F11/*", # Control or safety arrangements
+    "F24F3/*", # Air-conditioning systems in which conditioned primary air is supplied from one or
+    # more central stations to distributing units in the rooms or spaces where it may receive
+    # secondary treatment; Apparatus specially designed for such systems
+    "F25B*", # REFRIGERATION MACHINES, PLANTS OR SYSTEMS
+    "F25D*", # REFRIGERATORS; COLD ROOMS
+    "G06F1/*", # Compute–cooling coordination
+    "G06F1/32*",
+    "G06F1/26*",
+    "Y02B30/*", # Energy efficient heating, ventilation or air conditioning
+    "C09K*", # Materials / long-run innovation
+    "C23C*", # COATING METALLIC MATERIAL; COATING MATERIAL WITH METALLIC MATERIAL
+    "H01L23/*", # Materials / long-run innovation
+    "H01L27/*", # Materials / long-run innovation
+    "H05K7/*", # Cooling of electronic equipment
+    "H10D*", # Energy efficiency
+]
+
 NAMESPACES = {
     'uscom': 'urn:us:gov:doc:uspto:common',
     'uspat': 'urn:us:gov:doc:uspto:patent',
